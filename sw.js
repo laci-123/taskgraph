7a4ae9c3530ba9461329fbcabd1e418c6343a4f4
Gohér László <goher.laci@gmail.com>
2024-04-14 16:33:41 +0200

    clear caches on service worker update

--- sw.js
+++ sw.js
@@ -1,27 +1,32 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1");
-  await cache.addAll(resources);
+const updateCache = async (resources) => {
+    const cache_keys = await caches.keys();
+    for(const key of cache_keys) {
+        caches.delete(key);
+    }
+    
+    const cache = await caches.open("v3");
+    await cache.addAll(resources);
 };
 
 const cacheFirst = async (request) => {
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-  return fetch(request);
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    return fetch(request);
 };
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-      addResourcesToCache(["/",
-                           "/index.html",
-                           "/bundle.js",
-                           "css/default.css",
-                           "img/logo192x192.png",
-                           "img/logo512x512.png"]),
-  );
+    event.waitUntil(
+        updateCache(["/",
+                     "/index.html",
+                     "/bundle.js",
+                     "css/default.css",
+                     "img/logo192x192.png",
+                     "img/logo512x512.png"]),
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(cacheFirst(event.request));
+    event.respondWith(cacheFirst(event.request));
 });
